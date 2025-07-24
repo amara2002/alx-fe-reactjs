e2b42c23a669ff45ac805dfeabbcd24924dc381b
Amara Fuad Nyei <nyeiamara1@gmail.com>
2025-07-24 15:53:52 +0300

    Add detailed recipe management: view, edit, delete with routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,32 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route
-          path="/recipes/:id"
-          element={<RecipeDetailsWrapper />}
-        />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/recipe/:id/edit" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-};
-
-// Wrapper component to read recipeId from URL param
-import { useParams } from 'react-router-dom';
-
-const RecipeDetailsWrapper = () => {
-  const { id } = useParams();
-  const recipeId = Number(id);
-  return <RecipeDetails recipeId={recipeId} />;
-};
+}
 
 export default App;
