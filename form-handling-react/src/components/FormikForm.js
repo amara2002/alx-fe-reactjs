123fad52b3d85726440d1f164b734cbc1f80eda3
Amara Fuad Nyei <nyeiamara1@gmail.com>
2025-08-19 11:42:48 +0300

    Form Handling

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -4,7 +4,9 @@ import * as Yup from "yup";
 const validationSchema = Yup.object({
   username: Yup.string().required("Username is required"),
   email: Yup.string().email("Invalid email").required("Email is required"),
-  password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required")
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
 });
 
 export default function FormikForm() {
@@ -42,7 +44,11 @@ export default function FormikForm() {
             placeholder="Password"
             className="block border p-2 w-full mb-2"
           />
-          <ErrorMessage name="password" component="p" className="text-red-500" />
+          <ErrorMessage
+            name="password"
+            component="p"
+            className="text-red-500"
+          />
 
           <button type="submit" className="bg-green-500 text-white px-4 py-2">
             Register
